7a1ea06faf4c87af8458daf29162ecd60adeedb1
Jelle van den Hooff <jelle@vandenhooff.name>
2026-02-17 00:13:49 -0800

    all: add wasm32 to build constraints across the tree (WIP)
    Update build constraints in packages that distinguish wasm from other
    architectures. Files that exclude wasm (like lock_spinbit.go,
    mem_nonsbrk.go, stubs_nonwasm.go) now also exclude wasm32. Files that
    include wasm for architecture-specific behavior (like mem_sbrk.go,
    hash32.go, mpagealloc_32bit.go, tagptr_32bit.go) now also include
    wasm32.
    
    Disable DWARF generation for wasm32 (same as wasm) since the wasm
    binary format doesn't use standard DWARF, and disable FIPS for wasm32
    (same as wasm).
    
    For #63131
    
    Change-Id: Ifee22bfaaa61083f91a5387819e5ad35bded6d16

--- src/internal/runtime/atomic/atomic_andor_generic.go
+++ src/internal/runtime/atomic/atomic_andor_generic.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build arm || wasm
+//go:build arm || wasm || wasm32
 
 // Export some functions via linkname to assembly in sync/atomic.
 //
